50072de0ba4efe20ea44239a4b39dab3b7b1f2f3
xuzhang3 <Zhangxu894765>
2020-10-29 16:39:51 +0800

    package test

--- release.sh
+++ release.sh
@@ -46,10 +46,13 @@ function release() {
     info "GOOS: ${OS}, GOARCH: ${ARCH}"
     (
       env GOOS="${OS}" GOARCH="${ARCH}" go build -o "${BUILD_DIR}${BUILD_ARTIFACT}"
-      zip -r "${BUILD_DIR}${BUILD_ARTIFACT}_${VERSION}_${OS}_${ARCH}.zip" "${BUILD_DIR}${BUILD_ARTIFACT}"
-#      tar -cf "${BUILD_DIR}${BUILD_ARTIFACT}_${VERSION}_${OS}_${ARCH}.tar" "${BUILD_DIR}${BUILD_ARTIFACT}"
+      zip -r "${BUILD_DIR}${BUILD_ARTIFACT}_${OS}_${ARCH}.zip" "${BUILD_DIR}${BUILD_ARTIFACT}"
+#      tar -cf "${BUILD_DIR}${BUILD_ARTIFACT}_${OS}_${ARCH}.tar" "${BUILD_DIR}${BUILD_ARTIFACT}"
     )
   done
+  ls -al ${BUILD_DIR}/*.tar
+  shasum -a 256 ${BUILD_DIR}/*.tar > "./dist/${BUILD_ARTIFACT}_SHA256SUMS"
+  ls -al ${BUILD_DIR}/*.tar
 
 }
 
